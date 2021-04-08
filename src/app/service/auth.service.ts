8aca285f412a1ac7749fcca99cb1217a5d53c022
Francine <francine.massaro@gmail.com>
2021-04-08 09:42:13 -0300

    Componente inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/user';
 import { UserLogin } from '../model/userLogin';
 
@@ -18,4 +19,18 @@ export class AuthService {
   cadastrar (user:User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
+
+  //Pra saber se o token está preenchido, ou seja, quando clica em entrar
+  logado(){
+      let ok: boolean = false
+
+      //começa com a execução. Ok é falso.
+      //Se o enviroment.token for ! = diferente de vazio ele muda o ok para true
+      if (environment.token != ''){
+        ok=true
+      }
+
+      return ok
+  }
+
 }
